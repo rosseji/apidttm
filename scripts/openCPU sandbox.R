50c3289d434bfc3dc86d44dc364716f46648c6db
rosseji <rosseireland@gmail.com>
2018-09-04 16:56:49 +1000

    bump

--- scripts/openCPU sandbox.R
+++ scripts/openCPU sandbox.R
@@ -2,6 +2,31 @@
 library(httr)
 library(magrittr)
 
+
+response <- POST(
+  url = "https://cloud.opencpu.org/ocpu/github/rosseji/apidttm/R/test/json",
+  body = list(x = 5, y = 100213)
+)
+response$url
+response$status_code
+response$url
+
+
+response$content %>% rawToChar() %>% jsonlite::fromJSON()
+
+
+
+response <- GET(
+  url = "https://cloud.opencpu.org/ocpu/github/trendlock/trendlock",
+)
+
+
+response <- POST(
+  url = "https://cloud.opencpu.org/ocpu/github/rosseji/apidttm/R/test/json",
+  body = list(x = 5, y = 100213),
+)
+
+response$content %>% rawToChar() %>% jsonlite::fromJSON()
 response <- POST(
   url = "https://cloud.opencpu.org/ocpu/github/rosseji/apidttm/R/test"
 )
